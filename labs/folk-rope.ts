049ede88fdc5804ab4ac7e473d87cf6319692584
“chrisshank” <chris.shank.23@gmail.com>
2025-01-21 22:11:52 -0800

    add presentation

--- labs/folk-rope.ts
+++ labs/folk-rope.ts
@@ -40,7 +40,7 @@ export class FolkRope extends FolkBaseConnection implements AnimationFrameContro
 
       path {
         fill: none;
-        pointer-events: all;
+        pointer-events: stroke;
         stroke: var(--folk-rope-color, black);
         stroke-width: var(--folk-rope-width, 3);
         stroke-linecap: var(--folk-rope-linecap, round);
