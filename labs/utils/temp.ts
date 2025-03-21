97a007b14b936428b7ac86fe8d77e389836fa681
Orion Reed <orionreedt@gmail.com>
2025-03-21 03:00:59 -0400

    cleanup

--- labs/utils/temp.ts
+++
@@ -1,9 +0,0 @@
-import { header } from './header';
-
-const qrtpHeader = header('Q<indices:list-2>');
-const encoded = qrtpHeader.encode({
-  indices: ['ab', 'qw', 'wldfs'],
-});
-console.log({ encoded });
-const decoded = qrtpHeader.decode('Q05552rs93jdh');
-console.log({ decoded });
