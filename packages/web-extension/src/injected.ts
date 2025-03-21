6b20b28d609162d3d40635137505e42c94004a26
Orion Reed <orionreedt@gmail.com>
2025-03-21 03:32:13 -0400

    break extention so it builds :)

--- packages/web-extension/src/injected.ts
+++ packages/web-extension/src/injected.ts
@@ -1,16 +1,18 @@
-import { FolkShapeAttribute } from '@folk-systems/labs/folk-shape-attribute.ts';
-import { FolkZoomable } from '@folk-systems/labs/folk-zoomable.ts';
+// TODO: fix imports by finishing package migration
 
-FolkShapeAttribute.define();
-FolkZoomable.define();
+// import { FolkShapeAttribute } from '@folk-systems/labs/folk-shape-attribute.ts';
+// import { FolkZoomable } from '@folk-systems/labs/folk-zoomable.ts';
 
-document.documentElement.setAttribute('folk-zoomable', '');
+// FolkShapeAttribute.define();
+// FolkZoomable.define();
 
-document.body.addEventListener('dblclick', (e) => {
-  const el = e.target as HTMLElement;
-  // don't allow nesting of shapes
-  if (el.closest('[folk-shape]') === null && el.querySelector('[folk-shape]') === null) {
-    el.setAttribute('folk-shape', `width: ${el.clientWidth}`);
-    requestAnimationFrame(() => el.focus());
-  }
-});
+// document.documentElement.setAttribute('folk-zoomable', '');
+
+// document.body.addEventListener('dblclick', (e) => {
+//   const el = e.target as HTMLElement;
+//   // don't allow nesting of shapes
+//   if (el.closest('[folk-shape]') === null && el.querySelector('[folk-shape]') === null) {
+//     el.setAttribute('folk-shape', `width: ${el.clientWidth}`);
+//     requestAnimationFrame(() => el.focus());
+//   }
+// });
