ebb12bb1254197f79962a1e1d87baac94dc60189
“chrisshank” <chris.shank.23@gmail.com>
2025-03-25 20:31:02 -0700

    move benchmark script

--- packages/repo-utils/src/index.ts
+++ packages/repo-utils/src/index.ts
@@ -3,5 +3,3 @@ export { B, bench, group, measure } from 'mitata';
 
 // Export our custom run function
 export { runBenchmarks as run } from './runMitataBenchmark.js';
-
-export * from './benchmark.ts';
