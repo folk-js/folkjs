82dd48ab566704ac50e1da23e8c4271c8f35bb2e
“chrisshank” <chris.shank.23@gmail.com>
2025-03-25 23:17:47 -0700

    update benchmarks

--- packages/geometry/__benchmarks__/BVH.bench.ts
+++ packages/geometry/__benchmarks__/BVH.bench.ts
@@ -1,4 +1,4 @@
-import { bench, run } from '@folkjs/repo-utils';
+import { bench, do_not_optimize, run } from '@folkjs/repo-utils';
 import type { BVHNode } from '../src/BoundingVolumeHierarchy.ts';
 import * as BVH from '../src/BoundingVolumeHierarchy.ts';
 import type { Rect2D } from '../src/Rect2D.ts';
@@ -13,52 +13,52 @@ function createRandomShapes(length: number): Rect2D[] {
 }
 
 const shapes3 = createRandomShapes(3);
-let bvh3: BVH.BVHNode;
 
 bench('BVH: instantiate 3 shapes', () => {
-  bvh3 = BVH.fromRects(shapes3);
-});
+  do_not_optimize(BVH.fromRects(shapes3));
+}).gc('inner');
 
+const bvh3 = BVH.fromRects(shapes3);
 bench('BVH: collsion 3 shapes', () => {
-  BVH.intersections(bvh3, shapes3[0]);
-});
+  do_not_optimize(BVH.intersections(bvh3, shapes3[0]));
+}).gc('inner');
 
 const shapes100 = createRandomShapes(100);
 let bvh100: BVHNode;
 
 bench('BVH: instantiate 100 shapes', () => {
-  bvh100 = BVH.fromRects(shapes100);
-});
+  do_not_optimize((bvh100 = BVH.fromRects(shapes100)));
+}).gc('inner');
 
 bench('BVH: check collsion 100 shapes', () => {
-  BVH.intersections(bvh100, shapes100[0]);
-});
+  do_not_optimize(BVH.intersections(bvh100, shapes100[0]));
+}).gc('inner');
 
 const shapes1000 = createRandomShapes(1000);
 let bvh1000: BVHNode;
 
 bench('BVH: instantiate 1000 shapes', () => {
-  bvh1000 = BVH.fromRects(shapes1000);
-});
+  do_not_optimize((bvh1000 = BVH.fromRects(shapes1000)));
+}).gc('inner');
 
 bench('BVH: check collsion 1000 shapes', () => {
-  BVH.intersections(bvh1000, shapes1000[0]);
-});
+  do_not_optimize(BVH.intersections(bvh1000, shapes1000[0]));
+}).gc('inner');
 
 const shapes10000 = createRandomShapes(10000);
 let bvh10000: BVHNode;
 
 bench('BVH: instantiate 10000 shapes', () => {
-  bvh10000 = BVH.fromRects(shapes10000);
-});
+  do_not_optimize((bvh10000 = BVH.fromRects(shapes10000)));
+}).gc('inner');
 
 bench('BVH: check collsion 10000 shapes', () => {
-  BVH.intersections(bvh10000, shapes10000[0]);
-});
+  do_not_optimize(BVH.intersections(bvh10000, shapes10000[0]));
+}).gc('inner');
 
 bench('BVH: instantiate 10000 shapes and check one collision', () => {
   const bvh10000 = BVH.fromRects(shapes10000);
-  BVH.intersections(bvh10000, shapes10000[0]);
-});
+  do_not_optimize(BVH.intersections(bvh10000, shapes10000[0]));
+}).gc('inner');
 
 await run();
--- packages/repo-utils/src/index.ts
+++ packages/repo-utils/src/index.ts
@@ -1,5 +1,5 @@
 // Re-export everything from mitata except 'run'
-export { B, bench, group, measure } from 'mitata';
+export { B, bench, do_not_optimize, group, measure } from 'mitata';
 
 // Export our custom run function
 export { runBenchmarks as run } from './runMitataBenchmark.js';
