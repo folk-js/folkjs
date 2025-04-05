b6209b1804b00dd70937ab2583bc58cedf677d59
“chrisshank” <chris.shank.23@gmail.com>
2025-04-04 23:22:09 -0700

    update hash modules

--- packages/canvas/src/hash-modules.ts
+++ packages/canvas/src/hash-modules.ts
@@ -1,20 +1,33 @@
 // Ported from https://github.com/tomlarkworthy/lopecode
-const imports: Record<string, string> = {};
 
-document.querySelectorAll<HTMLScriptElement>('script[type=hash-module]').forEach((module) => {
-  imports['#' + module.id] = URL.createObjectURL(new Blob([module.text], { type: 'application/javascript' }));
-  module.remove();
-});
+let importmap: HTMLScriptElement | null = null;
+let mainScript: HTMLScriptElement | null = null;
 
-const importmap = document.createElement('script');
-importmap.type = 'importmap';
-importmap.text = JSON.stringify({ imports }, null, 2);
-document.head.appendChild(importmap);
+export function execute() {
+  cleanup();
 
-// We need the entry point of all JS modules that import hash modules to also be a hash module in order to guarantee this importmap is generated before the other modules run.
-if (imports['#main']) {
-  const mainScript = document.createElement('script');
-  mainScript.type = 'module';
-  mainScript.text = 'import "#main"';
-  document.head.appendChild(mainScript);
+  const imports: Record<string, string> = {};
+
+  document.querySelectorAll<HTMLScriptElement>('script[type=hash-module]').forEach((module) => {
+    imports['#' + module.id] = URL.createObjectURL(new Blob([module.text], { type: 'application/javascript' }));
+    module.remove();
+  });
+
+  importmap = document.createElement('script');
+  importmap.type = 'importmap';
+  importmap.text = JSON.stringify({ imports }, null, 2);
+  document.head.appendChild(importmap);
+
+  // We need the entry point of all JS modules that import hash modules to also be a hash module in order to guarantee this importmap is generated before the other modules run.
+  if (imports['#main']) {
+    mainScript = document.createElement('script');
+    mainScript.type = 'module';
+    mainScript.text = 'import "#main"';
+    document.head.appendChild(mainScript);
+  }
+}
+
+export function cleanup() {
+  importmap?.remove();
+  mainScript?.remove();
 }
