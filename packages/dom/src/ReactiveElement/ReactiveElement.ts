c70d9d079574dd8d049ea1231580e3746ec8706e
“chrisshank” <chris.shank.23@gmail.com>
2025-07-28 10:33:26 -0700

    add text-to-fit element

--- packages/dom/src/ReactiveElement/ReactiveElement.ts
+++ packages/dom/src/ReactiveElement/ReactiveElement.ts
@@ -310,13 +310,16 @@ export class ReactiveElement
   /** Defines the name of the custom element, must include a hyphen or it will error out when defined.
    * Defaults to a kebab-case version of the PascalCase class name
    */
-  static tagName = this.name
-    .replace(/([A-Z])/g, '-$1')
-    .toLowerCase()
-    .slice(1);
+  static tagName = '';
 
   /** Defines the custom element with the global CustomElementRegistry, ignored if called more than once. Errors if no tagName is defined or it doesn't include a hyphen. */
   static define() {
+    if (!this.tagName)
+      this.tagName = this.name
+        .replace(/([A-Z])/g, '-$1')
+        .toLowerCase()
+        .slice(1);
+
     if (customElements.get(this.tagName)) return;
 
     customElements.define(this.tagName, this);
