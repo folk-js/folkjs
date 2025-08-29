c943ad6fc75f8656f2894c2039c3ca74889fdd9b
“chrisshank” <chris.shank.23@gmail.com>
2025-08-29 20:17:18 +0200

    fix proximity util

--- packages/geometry/src/Rect2D.ts
+++ packages/geometry/src/Rect2D.ts
@@ -100,7 +100,7 @@ export function proximal(rect1: ReadonlyRect2D, rect2: ReadonlyRect2D, proximity
     rect1.x - (rect2.x + rect2.width) < proximity &&
     rect2.x - (rect1.x + rect1.width) < proximity &&
     rect1.y - (rect2.y + rect2.height) < proximity &&
-    rect2.y - (rect1.x + rect1.width) < proximity
+    rect2.y - (rect1.y + rect1.height) < proximity
   );
 }
 
